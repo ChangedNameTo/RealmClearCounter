382c2d949a0200c88eebaf8c1ded1651eec43c92
Will Mitchell <william.m95@gmail.com>
2020-01-15 15:37:14 -0500

    Initial commit

---
+++ clears.md
@@ -0,0 +1,25 @@
+|Rank|Realm|Clears|
+|1|Gilgamesh (NA)|202|
+|2|Leviathan (NA)|186|
+|3|Cactuar (NA)|174|
+|4|Sargatanas (NA)|172|
+|5|Behemoth (NA)|147|
+|6|Excalibur (NA)|133|
+|7|Exodus (NA)|116|
+|8|Adamantoise (NA)|112|
+|9|Hyperion (NA)|108|
+|10|Ultros (NA)|107|
+|11|Jenova (NA)|106|
+|12|Faerie (NA)|103|
+|13|Midgardsormr (NA)|93|
+|14|Lamia (NA)|86|
+|15|Balmung (NA)|72|
+|16|Siren (NA)|72|
+|17|Famfrit (NA)|68|
+|18|Brynhildr (NA)|67|
+|19|Malboro (NA)|64|
+|20|Coeurl (NA)|58|
+|21|Zalera (NA)|57|
+|22|Diabolos (NA)|57|
+|23|Goblin (NA)|56|
+|24|Mateus (NA)|49|
---
+++ totals.py
@@ -0,0 +1,38 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+page_num = 1
+next_link = True
+
+clear_dict = {}
+
+while next_link:
+    request = urllib.request.Request('https://www.fflogs.com/zone/rankings/table/29/progress/-1/101/8/1/Any/Any/0/0/1/0/0?page=' + str(page_num))
+    response = urllib.request.urlopen(request)
+    page_html = response.read()
+
+    soup = BeautifulSoup(page_html, 'html.parser')
+    clears = soup.findAll('a', {'class':'main-table-realm'})
+    navigation = soup.findAll('li', {'class':'page-item'})
+
+    for clear in clears:
+        realm = clear.text
+        if realm in clear_dict:
+            clear_dict[realm] += 1
+        else:
+            clear_dict[realm] = 1
+
+    for nav in navigation:
+        if 'Next' in nav.text:
+            if nav.has_attr('aria-disabled'):
+                next_link = False
+    page_num += 1
+
+clear_dict = {k: v for k, v in sorted(clear_dict.items(), key=lambda item: item[1], reverse=True)}
+
+with open('clears.md', 'w') as file:
+    file.write("|Rank|Realm|Clears|\n")
+    counter = 1
+    for realm in clear_dict.items():
+        file.write("|" + str(counter) + "|" + str(realm[0]) + "|" + str(realm[1]) + "|\n")
+        counter += 1
